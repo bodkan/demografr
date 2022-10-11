11be290394642c987529305ce4b1604b91da0454
Martin Petr <git@bodkan.net>
2022-10-11 11:35:35 +0200

    Fix subsetting the abc object for plotting

--- R/viz.R
+++ R/viz.R
@@ -78,7 +78,8 @@ hist.demografr_abc <- function(x, param = NULL, ...) {
     check_param_presence(colnames(params), param)
     x$numparam <- 1
     x$names$parameter.names <- param
-    params <- params[, param]
+    x$adj.values <- x$adj.values[, param, drop = FALSE]
+    x$unadj.values <- x$unadj.values[, param, drop = FALSE]
   }
   abc:::hist.abc(x, ...)
 }
