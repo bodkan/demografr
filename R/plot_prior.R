a93d887590b9fb0e0bc042eb23c3740b1e4c4b58
Martin Petr <git@bodkan.net>
2023-05-01 10:52:07 +0200

    Add optional output argument for plotting

--- R/plot_prior.R
+++ R/plot_prior.R
@@ -7,28 +7,30 @@
 #' @param replicates How many samples to simulate from each prior for plotting?
 #' @param geom Either \code{ggplot2::geom_histogram} or \code{ggplot2::geom_density}
 #'
-#' @import ggplot2
 #' @export
 plot_prior <- function(x, param = NULL, replicates = 10000, geom = ggplot2::geom_density,
-                       facets = FALSE) {
-  priors <- if (inherits(x, "demografr_abc")) attr(x, "priors") else x
+                       facets = FALSE, file = NULL, ...) {
+  priors <- if (inherits(x, "demografr_abc.abc")) attr(x, "priors") else x
   all_params <- get_prior_names(priors); names(priors) <- all_params
   subset_params <- subset_parameters(subset = param, all = all_params)
   priors <- priors[subset_params]; unname(priors)
 
   samples_df <- simulate_priors(priors, replicates)
 
-  p_facet <- if (facets) facet_wrap(~ param, scales = "free") else NULL
+  p_facet <- if (facets) ggplot2::facet_wrap(~ param, scales = "free") else NULL
 
-  p <- ggplot(samples_df) +
-    geom(aes(value, fill = param, color = param), alpha = 0.5) +
-    scale_fill_discrete(drop = FALSE) +
-    scale_color_discrete(drop = FALSE) +
-    theme_minimal() +
-    theme(strip.text.x = element_text(face = "bold", size = 13)) +
+  p <- ggplot2::ggplot(samples_df) +
+    geom(ggplot2::aes(value, fill = param, color = param), alpha = 0.5) +
+    ggplot2::scale_fill_discrete(drop = FALSE) +
+    ggplot2::scale_color_discrete(drop = FALSE) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(strip.text.x = ggplot2::element_text(face = "bold", size = 13)) +
     p_facet
 
-  p
+  if (!is.null(file))
+    ggplot2::ggsave(file, plot = p, ...)
+  else
+    p
 }
 
 simulate_priors <- function(priors, replicates = 1000) {
