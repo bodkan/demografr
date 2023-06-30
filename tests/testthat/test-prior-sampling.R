5d2fdbc25494572c1994a7b637351efbebc81231
Martin Petr <git@bodkan.net>
2023-06-30 15:18:40 +0200

    Prevent creating a useless Rplots.pdf file in tests

--- tests/testthat/test-prior-sampling.R
+++ tests/testthat/test-prior-sampling.R
@@ -9,7 +9,7 @@ test_that("Incorrect prior formula fails gracefully (warnings not allowed)", {
 })
 
 test_that("Incorrect prior formula fails gracefully (errors not allowed)", {
-  expect_error(sample_prior(pop ~ plot(10, 1)), "A warning was raised")
+  expect_error(sample_prior(pop ~ warning()), "A warning was raised")
   expect_true(is.list(sample_prior(pop ~ runif(1, 10))))
 })
 
