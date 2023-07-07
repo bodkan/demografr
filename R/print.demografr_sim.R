b9b3802103ac9a7373e05758fc7a6ee82d4b73e1
Martin Petr <git@bodkan.net>
2023-07-07 15:19:00 +0200

    Update printing

--- R/print.demografr_sim.R
+++ R/print.demografr_sim.R
@@ -28,8 +28,9 @@ print.demografr_sims <- function(x, ...) {
   cat("\n")
 
   cat("Individual components of this object can be accessed as:\n")
-  cat("    <object>$parameters -- parameter matrix\n")
-  cat("    <object>$observed   -- observed summary statistics\n")
-  cat("    <object>$functions  -- summary functions\n")
   cat("    <object>$priors     -- prior expressions\n")
+  cat("    <object>$parameters -- sampled parameters\n")
+  cat("    <object>$functions  -- summary functions\n")
+  cat("    <object>$observed   -- observed summary statistics\n")
+  cat("    <object>$simulated  -- simulated summary statistics\n")
 }
