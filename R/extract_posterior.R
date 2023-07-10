1bf34efe6e927d810f735c37810cb4ca36bef4a6
Martin Petr <git@bodkan.net>
2023-07-10 22:00:19 +0200

    Take care of one-column parameter matrix not having colnames in abc

--- R/extract_posterior.R
+++ R/extract_posterior.R
@@ -1,4 +1,3 @@
-
 #' Extract inferred posterior(s) as a standard data frame
 #' @export
 extract_posterior <- function(abc, posterior = c("adj", "unadj")) {
@@ -7,6 +6,8 @@ extract_posterior <- function(abc, posterior = c("adj", "unadj")) {
 
   # get the entire posterior sample, convert it to a long format, subset variables
   df <- abc[[paste0(posterior, ".values")]] %>%
+    as.data.frame() %>%
+    stats::setNames(colnames(attr(abc, "parameters"))) %>%
     dplyr::as_tibble() %>%
     tidyr::pivot_longer(cols = dplyr::everything(), names_to = "param", values_to = "value") %>%
     dplyr::filter(param %in% param)
