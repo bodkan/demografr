59e76a935bb6cbf5cfddd40de09f6f0c5b1b0687
Martin Petr <bodkan@users.noreply.github.com>
2023-07-22 17:27:20 +0200

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,5 +1,5 @@
 # 2022-02-16 13:32
 
-install.packages(c("sf", "stars", "rnaturalearth"))
+install.packages(c("slendr", "sf", "stars", "rnaturalearth"))
 
 slendr::setup_env(pip = TRUE, agree = TRUE)
