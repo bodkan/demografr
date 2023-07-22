ad09c40b64c9eb7fd536a2a7ef04cfe4e0565c63
Martin Petr <bodkan@users.noreply.github.com>
2023-07-22 18:13:51 +0200

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,5 +1,7 @@
 # 2022-02-16 13:32
 
-install.packages(c("slendr", "sf", "stars", "rnaturalearth"))
+install.packages(c("devtools", "slendr", "sf", "stars", "rnaturalearth"))
 
 slendr::setup_env(pip = TRUE, agree = TRUE)
+
+devtools::install_github("bodkan/demografr")
