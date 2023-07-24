154b657662f37ade1586421f2e9757bcafe90a9a
Martin Petr <git@bodkan.net>
2023-07-24 20:06:40 +0200

    Expand plotting in plot_prediction()

--- R/plot_prediction.R
+++ R/plot_prediction.R
@@ -49,8 +49,12 @@ plot_prediction <- function(data, stat = NULL, file = NULL, facets = TRUE, ...) 
   p <-
     ggplot2::ggplot(predicted_df, ggplot2::aes(value, color = !!color_by, fill = !!color_by)) +
       ggplot2::geom_density(alpha = 0.5) +
-      ggplot2::geom_vline(data = observed_df, ggplot2::aes(xintercept = value, color = !!color_by),
-                          linetype = "dashed")
+      ggplot2::geom_vline(data = observed_df, ggplot2::aes(xintercept = value, color = !!color_by, linetype = "observed")) +
+      ggplot2::theme(legend.position = "bottom") +
+      ggplot2::guides(color = ggplot2::guide_legend("simulated"),
+                      fill = ggplot2::guide_legend("simulated"),
+                      linetype = ggplot2::guide_legend("observed")) +
+      ggplot2::scale_linetype_manual(values = "dashed", labels = "")
 
   if (facets) p <- p + ggplot2::facet_wrap(~ stat, scales = scales)
 
