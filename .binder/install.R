f7219c366195e8b498776565c5c83f2817712881
Martin Petr <git@bodkan.net>
2023-07-24 17:24:00 +0200

    Use dev version of slendr on binder

--- .binder/install.R
+++ .binder/install.R
@@ -1,6 +1,8 @@
 # 2022-02-16 13:32
 
-install.packages(c("devtools", "slendr", "sf", "stars", "rnaturalearth"))
+install.packages(c("devtools", "sf", "stars", "rnaturalearth"))
+
+devtools::install_github("bodkan/slendr")
 
 slendr::setup_env(pip = TRUE, agree = TRUE)
 
