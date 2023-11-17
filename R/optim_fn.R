623f47b3afc999888f8c68d8be263063c38c22e9
Martin Petr <git@bodkan.net>
2023-11-17 11:41:48 +0000

    Add draft of GA optimization

---
+++ R/optim_fn.R
@@ -0,0 +1,56 @@
+optim_fn <- function(par, model, observed) {
+  # get all arguments of the model function...
+  all_args <- names(formals(model))
+  # ... from those, extract names of arguments which don't have an implicit value ...
+  nonimpl_args <- all_args[vapply(all_args, function(x) is.name(formals(model)[[x]]), logical(1))]
+  par <- as.list(par)
+  names(par) <- nonimpl_args
+  par <- as.data.frame(par)
+  print(par)
+
+  result <- tryCatch({
+    simulate_grid(model, par, functions, replicates = 1,
+                  sequence_length = 1e6, recombination_rate = 1e-8, mutation_rate = 1e-8)},
+    error = function(e) NULL
+  )
+
+  if (is.null(result)) {
+    rmse <- Inf
+  } else {
+    # name the list of data frames with simulated statistics
+    stat_names <- names(observed)
+    simulated <- lapply(stat_names, function(x) result[[x]][[1]])
+    names(simulated) <- stat_names
+
+    simulated_col <- vapply(stat_names,
+                            function(x) which(vapply(simulated[[x]], is.numeric, FUN.VALUE = logical(1))),
+                            FUN.VALUE = integer(1))
+    observed_col <- vapply(stat_names,
+                           function(x) which(vapply(observed[[x]], is.numeric, FUN.VALUE = logical(1))),
+                           FUN.VALUE = integer(1))
+
+    merged <- lapply(stat_names, function(stat) {
+      shared_cols <- colnames(observed[[stat]][, -observed_col[stat], drop = FALSE])
+      merged_stats <- dplyr::inner_join(
+        simulated[[stat]], observed[[stat]],
+        by = shared_cols
+      )
+      merged_stats[shared_cols] <- NULL
+      names(merged_stats) <- c("simulated", "observed")
+      merged_stats %>%
+        dplyr::mutate(
+          norm_sim = (simulated - mean(simulated)) / sd(simulated),
+          norm_obs = (observed - mean(observed)) / sd(observed)
+        )
+    }) %>%
+      do.call(rbind, .)
+
+    rmse <- sqrt(mean((merged$norm_sim - merged$norm_obs)^2))
+  }
+
+  # rmse = Inf (worse fit possible) => fitness = 0
+  # rmse = 0   (best fit possible)  => fitness = 100
+  fitness <- 100 / (1 + rmse)
+
+  fitness
+}
