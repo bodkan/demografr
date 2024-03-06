955b8332d870f16a3b9e899232e4f949309ccf50
Martin Petr <git@bodkan.net>
2024-03-06 16:02:23 +0000

    Make plot_prediction handle vectorized statistics (like SFS)

--- R/plot_prediction.R
+++ R/plot_prediction.R
@@ -2,18 +2,19 @@
 #'
 #' @param data Data frame with predictions generated from ABC posteriors, as generated
 #'   by the \code{predict} function
-#' @param stat Which tree-sequence summary statistic to extract? If \code{NULL}, all summary
-#'   statistics will be returned.
-#' @param file Output file for a figure saved via \code{ggsave}
+#' @param stat Which tree-sequence summary statistic to extract? If \code{NULL}, all
+#'   summary statistics will be visualized.
+#' @param file Output path to a PDF figure to be saved
 #' @param facets Should each summary statistic be plotted on its own facet (default is
 #'   \code{TRUE})?
 #' @param ... Optional argument which will be passed to \code{ggsave}
 #'
-#' @return If \code{file = NULL}, a ggplot2 object, otherwise nothing is returned and the
-#'   plot is instead saved to a file.
+#' @return If \code{file = NULL}, a ggplot2 object (or a list of such objects, if
+#'   multiple summary statistics are visualized), otherwise nothing is returned and the
+#'   plots are instead saved to a file.
 #'
 #' @export
-plot_prediction <- function(data, stat = NULL, file = NULL, facets = TRUE, ...) {
+plot_prediction <- function(data, stat, file = NULL, facets = TRUE, ...) {
   observed <- attr(data, "components")$observed
 
   all_stats <- names(observed)
@@ -22,47 +23,78 @@ plot_prediction <- function(data, stat = NULL, file = NULL, facets = TRUE, ...) 
   if (!all(stat %in% all_stats))
     stop("Unknown statistic(s) '", paste(stat, collapse = ", "), "' specified", call. = FALSE)
 
-  observed_df <-
-    lapply(
-      stat,
-      function(s) {
-        df <- observed[[s]]
+  p_stats <- list()
 
-        value_col <- vapply(df, is.numeric, FUN.VALUE = logical(1))
+  for (s in stat) {
+    is_vector_stat <- all(vapply(observed[[s]], is.numeric, FUN.VALUE = logical(1)))
 
-        stat_names <- colnames(df)[!value_col]
-        value_name <- colnames(df)[value_col]
+    if (is_vector_stat) {
+      observed_df <- observed[[s]]
+    } else {
+      observed_df <-
+        lapply(
+          s,
+          function(i) {
+            df <- observed[[i]]
 
-        dplyr::tibble(
-          summary = s,
-          stat = paste0(s, "_", apply(df[, stat_names, drop = FALSE], 1, paste, collapse = "_")),
-          value = df[, value_col]
-        )
-      }
-    ) %>% do.call(rbind, .)
+            value_col <- vapply(df, is.numeric, FUN.VALUE = logical(1))
 
-  predicted_df <- extract_prediction(data, stat)
+            stat_names <- colnames(df)[!value_col]
+            value_name <- colnames(df)[value_col]
 
-  color_by <- as.symbol(if (length(stat) > 1) "summary" else "stat")
-  scales <- if (length(stat) > 1) "free" else NULL
+            dplyr::tibble(
+              summary = i,
+              stat = paste0(i, "_", apply(df[, stat_names, drop = FALSE], 1, paste, collapse = "_")),
+              value = df[, value_col]
+            )
+          }
+        ) %>% do.call(rbind, .)
+    }
 
-  model_name <- attr(data, "components")$model_name
+    predicted_df <- extract_prediction(data, s)
 
-  p <-
-    ggplot2::ggplot(predicted_df, ggplot2::aes(value, color = !!color_by, fill = !!color_by)) +
-      ggplot2::geom_density(alpha = 0.5) +
-      ggplot2::geom_vline(data = observed_df, ggplot2::aes(xintercept = value, color = !!color_by, linetype = "observed")) +
-      ggplot2::theme(legend.position = "bottom") +
-      ggplot2::guides(color = ggplot2::guide_legend("simulated"),
-                      fill = ggplot2::guide_legend("simulated"),
-                      linetype = ggplot2::guide_legend("observed")) +
-      ggplot2::scale_linetype_manual(values = "dashed", labels = "") +
-      ggplot2::ggtitle(paste0("Posterior predictive check for model '", model_name, "'"))
+    if (is_vector_stat) {
+      x <- as.symbol(colnames(observed_df)[1])
+      y <- as.symbol(colnames(observed_df)[2])
+      p <-
+        ggplot() +
+          geom_line(data = predicted_df, aes(!!x, !!y, color = rep, group = rep), alpha = 0.5) +
+          geom_line(data = observed_df, aes(!!x, !!y, linetype = "observed"), color = "red") +
+          scale_linetype_manual(name = "observed", values = "dashed", labels = "") +
+          scale_color_continuous(name = "samples")
+    } else {
+      color_by <- as.symbol(if (length(s) > 1) "summary" else "stat")
+      scales <- if (length(stat) > 1) "free" else NULL
 
-  if (facets) p <- p + ggplot2::facet_wrap(~ stat, scales = scales)
+      p <-
+        ggplot(predicted_df, aes(value, color = !!color_by, fill = !!color_by)) +
+          geom_density(alpha = 0.5) +
+          geom_vline(data = observed_df, aes(xintercept = value, color = !!color_by, linetype = "observed")) +
+          guides(color = guide_legend("simulated"),
+                 fill = guide_legend("simulated"),
+                 linetype = guide_legend("observed")) +
+        scale_linetype_manual(name = "observed", values = "dashed", labels = "")
+    }
 
-  if (!is.null(file))
-    ggplot2::ggsave(file, plot = p, ...)
-  else
-    p
+    if (facets && !is_vector_stat) p <- p + ggplot2::facet_wrap(~ stat, scales = scales)
+
+    model_name <- attr(data, "components")$model_name
+    p <- p +
+      ggtitle(paste0("Posterior predictive check for model '", model_name, "' and statistic '", s, "'")) +
+      theme(legend.position = "bottom")
+
+    p_stats[[length(p_stats) + 1]] <- p
+  }
+  names(p_stats) <- stat
+
+  if (!is.null(file)) {
+    pdf(file, ...)
+    for (p in p_stats) print(p)
+    dev.off()
+  } else {
+    if (length(p_stats) == 1)
+      return(p_stats[[1]])
+    else
+      return(p_stats)
+  }
 }
