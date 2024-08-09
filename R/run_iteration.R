7f9a8dbba15e2265a0d7fe446c831c0261a85c13
Martin Petr <git@bodkan.net>
2024-08-09 17:35:00 +0200

    Implement new data changes into run_iteration()

--- R/run_iteration.R
+++ R/run_iteration.R
@@ -2,38 +2,58 @@
 run_iteration <- function(it,
                           model, params, functions,
                           sequence_length, recombination_rate, mutation_rate,
-                          engine, model_args, engine_args,
+                          data, format, engine, model_args, engine_args,
                           model_name, attempts) {
-  init_env(quiet = TRUE)
+  if (format == "files" && missing(data_funs))
+    stop("Models which generate custom files require a list of data function(s)\n",
+         "which will process them for computation of summary statistics.", call. = FALSE)
 
-  sim_result <- run_simulation(
+  data_expr <- base::substitute(data)
+  if (is.symbol(data_expr))
+    data_expr <- data
+
+  if (format == "ts")
+    validate_user_functions(data_expr, valid_args = c("ts", "model"))
+  else
+    validate_user_functions(data_expr, valid_args = c("path", "model"))
+
+  init_env(quiet = TRUE)
+  result <- run_simulation(
     model = model, params = params, sequence_length = sequence_length,
-    recombination_rate = recombination_rate, mutation_rate = mutation_rate,
+    recombination_rate = recombination_rate,
     model_name = model_name, engine = engine, samples = samples,
-    model_args = model_args, engine_args = engine_args,
+    model_args = model_args, engine_args = engine_args, format = format,
     attempts = attempts
   )
-  output <- sim_result$output
-  param_values <- sim_result$param_values
+  result_data <- result$data
+  result_params <- result$param_values
 
-  if (is.function(model)) {
+  if (format == "ts") {
     if (mutation_rate != 0)
-      ts <- slendr::ts_mutate(output, mutation_rate = mutation_rate)
+      result_data <- ts = slendr::ts_mutate(result_data, mutation_rate = mutation_rate)
 
     # clean up if needed
-    if (!is.null(attr(output, "path")))
-      unlink(attr(output, "path"))
+    if (!is.null(attr(result_data, "path")))
+      unlink(attr(result_data, "path"))
+
+    result_data <- list(ts = result_data)
+  }
+
+  # if user-defined generators were provided, apply each generator to the result
+  if (!is.null(data_expr)) {
+    env <- populate_data_env(result)
+    result_data <- evaluate_functions(data_expr, env)
   }
 
   # collect data for a downstream ABC inference:
   #   1. compute summary statistics using user-defined tree-sequence functions
-  simulated_stats <- lapply(functions, function(f) f(ts))
+  simulated_stats <- summarise_data(result_data, functions)
   #   2. collect all parameter values (sampled from priors or given) into a single parameter matrix
   if (contains_priors(params))
-    param_values <- collect_param_matrix(param_values)
+    result_params <- collect_param_matrix(result_params)
 
   list(
-    parameters = param_values,
+    parameters = result_params,
     simulated = simulated_stats
   )
 }
