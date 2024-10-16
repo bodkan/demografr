d26e61426de4678c784c6c07d5a28f0d82c653e7
Martin Petr <git@bodkan.net>
2024-10-16 16:39:09 +0000

    Make cross_validate() an S3 method

--- R/cross_validate.R
+++ R/cross_validate.R
@@ -12,18 +12,38 @@
 #' @return Object of the class \code{cv4postpr} and \code{demografr_cv}
 #'
 #' @export
-cross_validate <- function(models, nval, tols, method, ...) {
-  if (!all(vapply(models, inherits, "demografr_abc_sims", FUN.VALUE = logical(1))) &&
-      !all(vapply(models, inherits, "demografr_abc.abc", FUN.VALUE = logical(1))))
-    stop("The list of models must be either all objects produced by the function\n",
-         "`simulate_abc()` or function `run_abc()` as they store full information\n",
-         "about an ABC model being run, namely the simulated summary statistics", call. = FALSE)
+cross_validate <- function(x, nval, tols, method, ...) {
+  UseMethod("cross_validate")
+}
 
+#' @export
+cross_validate.list <- function(x, nval, tols, method, ...) {
+  args <- list(nval = nval, tols = tols, method = method, ...)
+  if (all(vapply(models, inherits, "demografr_abc.abc", FUN.VALUE = logical(1)))) {
+      args[["models"]] <- x
+    do.call(cross_validate_model_selection, args)
+  } else {
+    stop("The list of models must be all objects produced by the function `run_abc()", call. = FALSE)
+  }
+}
+
+cross_validate_model_selection <- function(models, nval, tols, method, ...) {
   parts <- unpack(models)
 
-  result <- abc::cv4postpr(index = parts$index, sumstat = parts$sumstat, nval = nval, tols = tols, method = method)
+  result <- abc::cv4postpr(index = parts$index, sumstat = parts$sumstat, nval = nval, tols = tols, method = method, ...)
 
-  class(result) <- c("demografr_cv", class(result))
+  class(result) <- c("demografr_cv_modsel", class(result))
+
+  result
+}
+
+#' @export
+cross_validate.demografr_abc.abc <- function(x, nval, tols, method, ...) {
+  parts <- unpack(x)
+
+  result <- abc::cv4abc(parts$param, parts$sumstat, nval = nval, tols = tols, method = method, ...)
+
+  class(result) <- c("demografr_cv_abc", class(result))
 
   result
 }
