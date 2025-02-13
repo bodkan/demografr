2b955400a8bd47a60378d6689dd098f9f231c9e5
Martin Petr <git@bodkan.net>
2025-02-13 12:19:03 +0000

    Add paper stuff

--- paper/paper.R
+++ paper/paper.R
@@ -4,14 +4,12 @@
 
   library(dplyr)
 
-  # set up parallelization across all CPUs on the current machine
   library(future)
   plan(multisession, workers = availableCores())
 
 
-  #--------------------------------------------------------------------------------
-  # define a model generating function using the slendr interface
-  # (each of the function parameters correspond to a parameter we want to infer)
+
+
 
   model <- function(Ne_A, Ne_B, Ne_C, T_AB, T_BC, gf_BC) {
     A <- population("popA", time = 10000, N = Ne_A)
@@ -28,6 +26,21 @@
     return(model)
   }
 
+
+
+  compute_diversity <- function(ts) { ... }
+  compute_divergence <- function(ts) { ... }
+  compute_f3 <- function(ts) { ... }
+
+  functions <- list(
+    diversity  = compute_diversity,
+    divergence = compute_divergence,
+    f3         = compute_f3
+  )
+
+
+
+
   example_model <- model(Ne_A = 2000, Ne_B = 800, Ne_C = 9000,
                          T_AB = 6000, T_BC = 4000, gf_BC = 0.1)
   plot_model(example_model, proportions = TRUE)
@@ -39,40 +52,38 @@
   observed_divergence <- ts_divergence(ts, sample_sets = samples) %>% as.data.frame()
   observed_f3 <- ts_f3(ts, samples["popA"], samples["popB"], samples["popC"]) %>% as.data.frame()
 
+
+
+
+
   observed <- list(
     diversity  = observed_diversity,
     divergence = observed_divergence,
-    f4         = observed_f3
+    f3         = observed_f3
   )
 
 
-  #--------------------------------------------------------------------------------
-  # setup priors for model parameters
 
   priors <- list(
-    Ne_A ~ runif(1, 10000),
-    Ne_B ~ runif(1, 10000),
-    Ne_C ~ runif(1, 10000),
+    Ne_A ~ runif(1000, 10000),
+    Ne_B ~ runif(100,  3000),
+    Ne_C ~ runif(7000, 10000),
 
     T_AB ~ runif(1, 10000),
     T_BC ~ runif(1, 10000),
 
-    gf_BC ~ runif(0, 1)
+    gf_BC ~ runif(0, 0.3)
   )
-
-  #--------------------------------------------------------------------------------
-  # define summary functions to be computed on simulated data (must be of the
-  # same format as the summary statistics computed on empirical data)
 
   compute_diversity <- function(ts) {
     samples <- ts_names(ts, split = "pop")
-    ts_diversity(ts, sample_sets = samples) %>% rename(pop = set) %>% as.data.frame()
+    ts_diversity(ts, sample_sets = samples) %>% dplyr::rename(pop = set) %>% as.data.frame()
   }
   compute_divergence <- function(ts) {
     samples <- ts_names(ts, split = "pop")
     ts_divergence(ts, sample_sets = samples)
   }
-  compute_f4 <- function(ts) {
+  compute_f3 <- function(ts) {
     samples <- ts_names(ts, split = "pop")
     ts_f3(ts, samples["popA"], samples["popB"], samples["popC"])
   }
@@ -80,44 +91,49 @@
   functions <- list(
     diversity  = compute_diversity,
     divergence = compute_divergence,
-    f4         = compute_f4
+    f3         = compute_f3
   )
+
+
+
 
   ts <- simulate_model(model, priors, sequence_length = 10e6, recombination_rate = 1e-8, mutation_rate = 1e-8)
   functions$diversity(ts)
-
   summarise_data(list(ts = ts), functions)
 
-  compute_diversity <- function(ts) { ... }
-  compute_divergence <- function(ts) { ... }
-  compute_f4 <- function(ts) { ... }
 
-  functions <- list(
-    diversity  = compute_diversity,
-    divergence = compute_divergence,
-    f4         = compute_f4
-  )
 
-  #--------------------------------------------------------------------------------
-  # validate the individual ABC components for correctness and consistency
+
+
+
+
+
+
   validate_abc(model, priors, functions, observed)
 
-  #--------------------------------------------------------------------------------
-  # run ABC simulations
+
+
+
+
+
+  data <- readRDS(file = here::here("inst/paper/abc_data.rds"))
+  tstart <- Sys.time()
 
   data <- simulate_abc(
     model, priors, functions, observed,
-    iterations = 100000,
-    sequence_length = 50e6, recombination_rate = 1e-8, mutation_rate = 1e-8
+    iterations = 10000,
+    sequence_length = 20e6, recombination_rate = 1e-8, mutation_rate = 1e-8
   )
+
+  saveRDS(data, file = here::here("inst/paper/abc_data.rds"))
+  tend <- Sys.time()
+  tend - tstart # Time difference of 47.32914 mins
+
 
   #--------------------------------------------------------------------------------
   # infer posterior distributions of parameters using the abc R package
 
   abc <- run_abc(data, tol = 0.01, method = "neuralnet")
-
-  data_path <- here::here("inst/examples/basics_abc.rds")
-  abc <- readRDS(data_path)
 
   # get table of posterior parameter estimates
 
@@ -125,18 +141,28 @@
 
   extract_summary(abc, "T")
 
+  extract_summary(abc, "gf")
 
-  plot_posterior(abc, param = "Ne")
 
-  plot_posterior(abc, param = c("Ne_A", "Ne_B", "Ne_C"))
+  plot_posterior(abc, param = "Ne") + ggplot2::coord_cartesian(xlim = c(0, 10000))
+
+  plot_posterior(abc, param = "T") + ggplot2::coord_cartesian(xlim = c(0, 10000))
+
+  plot_posterior(abc, param = "gf") + ggplot2::coord_cartesian(xlim = c(0, 1))
+
+
+  predictions <- readRDS(file = here::here("inst/paper/abc_predictions.rds"))
+  tstart <- Sys.time()
 
   predictions <- predict(abc, samples = 1000)
 
+  saveRDS(data, file = here::here("inst/paper/abc_predictions.rds"))
+  tend <- Sys.time()
+  tend - tstart # Time difference of 3.778034 mins
+
   plot_prediction(predictions, "diversity")
   plot_prediction(predictions, "divergence")
-  plot_prediction(predictions, "f3")
-
-
+  plot_prediction(predictions, "f3") + ggplot2::geom_vline(xintercept = 0, linetype = "dotted")
 
 
 
