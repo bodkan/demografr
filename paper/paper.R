3406bd755ba2a05af8de18468d1b7e73df590f2e
Martin Petr <git@bodkan.net>
2025-02-14 10:41:56 +0100

    Add paper draft bits

--- paper/paper.R
+++ paper/paper.R
@@ -12,9 +12,9 @@
 
 
   model <- function(Ne_A, Ne_B, Ne_C, T_AB, T_BC, gf_BC) {
-    A <- population("popA", time = 10000, N = Ne_A)
-    B <- population("popB", time = T_AB,  N = Ne_B, parent = A)
-    C <- population("popC", time = T_BC,  N = Ne_C, parent = B)
+    A <- population("A", time = 10000, N = Ne_A)
+    B <- population("B", time = T_AB,  N = Ne_B, parent = A)
+    C <- population("C", time = T_BC,  N = Ne_C, parent = B)
 
     gf <- gene_flow(B,  C, start = 3500, end = 3000, rate = gf_BC)
 
@@ -30,7 +30,10 @@
 
   compute_diversity <- function(ts) { ... }
   compute_divergence <- function(ts) { ... }
-  compute_f3 <- function(ts) { ... }
+  compute_f3 <- function(ts) {
+    samples <- ts_names(ts, split = "pop")
+    ts_f3(ts, samples["A"], samples["B"], samples["C"])
+  }
 
   functions <- list(
     diversity  = compute_diversity,
@@ -41,16 +44,16 @@
 
 
 
-  example_model <- model(Ne_A = 2000, Ne_B = 800, Ne_C = 9000,
+  true_model <- model(Ne_A = 2000, Ne_B = 800, Ne_C = 9000,
                          T_AB = 6000, T_BC = 4000, gf_BC = 0.1)
-  plot_model(example_model, proportions = TRUE)
-  ts <- msprime(example_model, sequence_length = 200e6, recombination_rate = 1e-8,
+  plot_model(true_model, proportions = TRUE)
+  ts <- msprime(true_model, sequence_length = 200e6, recombination_rate = 1e-8,
                 random_seed = 42) %>% ts_mutate(1e-8, random_seed = 42)
 
   samples <- ts_names(ts, split = "pop")
   observed_diversity <- ts_diversity(ts, sample_sets = samples) %>% rename(pop = set) %>% as.data.frame()
   observed_divergence <- ts_divergence(ts, sample_sets = samples) %>% as.data.frame()
-  observed_f3 <- ts_f3(ts, samples["popA"], samples["popB"], samples["popC"]) %>% as.data.frame()
+  observed_f3 <- ts_f3(ts, samples["A"], samples["B"], samples["C"]) %>% as.data.frame()
 
 
 
@@ -61,6 +64,8 @@
     divergence = observed_divergence,
     f3         = observed_f3
   )
+
+  observed
 
 
 
@@ -85,7 +90,7 @@
   }
   compute_f3 <- function(ts) {
     samples <- ts_names(ts, split = "pop")
-    ts_f3(ts, samples["popA"], samples["popB"], samples["popC"])
+    ts_f3(ts, samples["A"], samples["B"], samples["C"])
   }
 
   functions <- list(
@@ -116,18 +121,18 @@
 
 
 
-  data <- readRDS(file = here::here("inst/paper/abc_data.rds"))
+  data <- readRDS(file = here::here("paper/abc_data.rds"))
   tstart <- Sys.time()
 
   data <- simulate_abc(
     model, priors, functions, observed,
     iterations = 10000,
-    sequence_length = 20e6, recombination_rate = 1e-8, mutation_rate = 1e-8
+    sequence_length = 10e6, recombination_rate = 1e-8, mutation_rate = 1e-8
   )
 
-  saveRDS(data, file = here::here("inst/paper/abc_data.rds"))
+  saveRDS(data, file = here::here("paper/abc_data.rds"))
   tend <- Sys.time()
-  tend - tstart # Time difference of 47.32914 mins
+  tend - tstart # Time difference of 33.32081 mins
 
 
   #--------------------------------------------------------------------------------
@@ -151,19 +156,62 @@
   plot_posterior(abc, param = "gf") + ggplot2::coord_cartesian(xlim = c(0, 1))
 
 
-  predictions <- readRDS(file = here::here("inst/paper/abc_predictions.rds"))
+  predictions <- readRDS(file = here::here("paper/abc_predictions.rds"))
   tstart <- Sys.time()
 
   predictions <- predict(abc, samples = 1000)
 
-  saveRDS(data, file = here::here("inst/paper/abc_predictions.rds"))
+  saveRDS(data, file = here::here("paper/abc_predictions.rds"))
   tend <- Sys.time()
-  tend - tstart # Time difference of 3.778034 mins
+  tend - tstart # Time difference of 33.32081 mins
 
   plot_prediction(predictions, "diversity")
+
   plot_prediction(predictions, "divergence")
+
+  plot_prediction(predictions, "f3")
+
+
+
+  plot_prediction(predictions, "diversity", facets = FALSE)
+  plot_prediction(predictions, "divergence", facets = FALSE)
   plot_prediction(predictions, "f3") + ggplot2::geom_vline(xintercept = 0, linetype = "dotted")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  library(tidyr)
+
+  grid <- crossing(
+    Ne_A = seq(1000, 5000, by = 1000),
+    Ne_B = seq(100, 1000, by = 100),
+    Ne_C = seq(5000, 10000, by = 1000)
+  )
+
+  head(grid)
+
+  grid <- data.frame(
+    Ne_A = ...,
+    Ne_B = ...,
+    Ne_C = ...,
+    ...
+  )
+
+
+  data <- simulate_grid(
+    model, grid, functions, replicates = 100,
+    sequence_length = 10e6, mutation_rate = 1e-8, recombination_rate = 1e-8
+  )
 
 
 
