e71c8ef6638eb9447117944f273bf24dcf54f469
Martin Petr <git@bodkan.net>
2025-03-08 09:06:20 +0100

    Revert logging

--- R/run_iteration.R
+++ R/run_iteration.R
@@ -18,7 +18,6 @@ run_iteration <- function(it,
     validate_user_functions(data_expr, valid_args = c("path", "model"))
 
   init_env(quiet = TRUE)
-  stop(reticulate::py_config())
   result <- run_simulation(
     model = model, params = params, sequence_length = sequence_length,
     recombination_rate = recombination_rate,
