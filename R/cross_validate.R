6756109b6c24c1f09599abc415c589336a7c2b56
Martin Petr <git@bodkan.net>
2025-06-30 10:51:18 +0200

    Add examples for cross_validate()

--- R/cross_validate.R
+++ R/cross_validate.R
@@ -3,13 +3,47 @@
 #' This function is a convenience wrapper around the \code{cv4postpr} from the
 #' R package abc
 #'
-#' @param x A list of objects of the class \code{demografr_sims_abc} or
-#'   \code{demografr_abc.abc} which store simulated summary statistics needed for
-#'   calling \code{abc::cv4postpr}
+#' @param x A list of objects of the class \code{demografr_sims_abc} (each
+#'   representing a model for model selection via the function abc::cv4postpr),
+#'   or a single object \code{demografr_abc.abc} to perform cross-validation
+#'   by calling the function \code{abc::cv4abc}.
 #' @param nval,tols,method Required arguments of \code{abc::cv4postpr}
 #' @param ... Other optional arguments to be passed to \code{abc::cv4postpr}
 #'
 #' @return Object of the class \code{cv4postpr} and \code{demografr_cv}
+#'
+#'
+#' @examples
+#' \dontshow{slendr::check_dependencies(python = TRUE, quit = TRUE) # dependencies must be present
+#' }
+#'
+#' ########################################
+#' # cross-validation
+#'
+#' # read an example result of an ABC inference
+#' abc_res <- readRDS(system.file("examples/basics_abc.rds", package = "demografr"))
+#' # perform cross-validation
+#' cv_res <- cross_validate(abc_res, nval = 10, tols = c(0.005, 0.01, 0.05), method = "neuralnet")
+#' cv_res
+#'
+#' ########################################
+#' # model selection
+#'
+#' # \dontrun{
+#' # read a list of three different ABC models
+#' models <- lapply(c("X", "Y", "Z"), function(i) {
+#'   readRDS(
+#'     url(
+#'       sprintf("https://raw.githubusercontent.com/bodkan/demografr/refs/heads/main/inst/examples/downstream_abc%s.rds", i)
+#'     )
+#'   )
+#' })
+#' # note that each element of the list is, indeed, a demografr ABC result
+#' models[[1]]
+#  # perform model selection (see the abc package vignette for interpretation)
+#' mod_sel <- cross_validate(models, nval = 10, tols = c(0.005, 0.01, 0.05), method = "neuralnet")
+#' mod_sel
+#' }
 #'
 #' @export
 cross_validate <- function(x, nval, tols, method, ...) {
