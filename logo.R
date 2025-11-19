733e9e2f612a54e659da327985f6c01db7d90ab1
Martin Petr <git@bodkan.net>
2025-11-19 15:57:48 +0100

    Change logo generation

--- logo.R
+++ logo.R
@@ -1,6 +1,6 @@
 library(hexSticker)
 
-sticker("logo.png", package = "demografr",
+sticker("drawing.png", package = "demografr",
         p_size=130, p_color = "black", p_y = 0.53, p_family = "sans", p_fontface = "bold.italic",
         h_fill = "white", h_color = "#4d4d4d", h_size = 3,
         s_x = 1, s_y = 1.22, s_width = 0.65, dpi = 2000,
