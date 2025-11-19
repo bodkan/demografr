f98562f181bfac98409aa054559741537a84834e
Martin Petr <git@bodkan.net>
2025-11-19 14:42:25 +0100

    Add yaml dependency

--- devdeps.R
+++ devdeps.R
@@ -1,4 +1,7 @@
+# these are development-specific packages, listed for the purpose of renv
+# discovering them during the container-building process
 library(devtools)
 library(pkgdown)
 library(rmarkdown)
 library(markdown)
+library(yaml)
